5210b5a6396f828b181f92a68e50baa4d77a9f24
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 14:56:11 +0000

    Automatically backed up by Learn

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -3,5 +3,15 @@ def smallest_multiple(limit)
   range = (1..limit)
   factors = [1] 
   range.each {|number| 
-    if 
+    if factors.find {|factor| number % factor == 0}
+      i = 0
+      reduced_number
+      while i < factors.length 
+        if reduced_number % factors[i] == 0 
+          reduced_number /= reduced_number / factors[i] 
+        end
+        i += 1
+    else
+      factors << number 
+    end
 end 
