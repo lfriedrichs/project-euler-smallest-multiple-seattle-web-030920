fa7de0e04b5c12c58ed7728d6b6737432b761cb7
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 14:50:51 +0000

    Automatically backed up by Learn

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,4 +1,7 @@
 # Implement your procedural solution here!
 def smallest_multiple(limit) 
-  1 * 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13 * 2 * 17 * 19 
+  range = (1..limit)
+  factors = [1] 
+  range.each {|number| 
+    if 
 end 
