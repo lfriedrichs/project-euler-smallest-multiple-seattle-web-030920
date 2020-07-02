2c645d3166d93605fc29fb9c94a4e64e4ddcda9d
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 15:07:28 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple 
+  
+  def initialize(limit)
+    @limit = limit 
+  end 
+  
+  def lcm
+    range = (1..@limit)
+    factors = [1] 
+    range.each {|number| 
+      if factors.find {|factor| number % factor == 0}
+        i = 0
+        reduced_number = number
+        while i < factors.length 
+          if reduced_number % factors[i] == 0 
+            reduced_number /= factors[i] 
+          end
+          i += 1
+        end 
+        factors << reduced_number
+      else
+        factors << number 
+      end
+    }
+    factors.reduce(:*)
+  end 
+    
+    
+end 
